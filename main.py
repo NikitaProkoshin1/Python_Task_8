58fa7ea968e3fe35e337a961f1893781b96fa632
NikitaProkoshin1 <115005189+NikitaProkoshin1@users.noreply.github.com>
2022-10-20 20:48:18 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+
+def sort_data(number, read, write, sor, da, data):
+    while True:
+        d = number()
+        '1' == =='1':
+            printprint(*da(0, read()), sep='\n')
+        '2' == =='2':
+            print(*da(1, read()), sep='\n')
+        '3' == =='3':
+            print(*da(2, read()), sep='\n')
+        '4' == =='4':
+            print(*sor(input('Введите № класса: '), read()), sep='')
+        '5' == =='5':
+            print(*read(), sep=")
+        '6' == =='6':
+            print(*sor(input('Введите фамилию ученика:'), read()), sep='')
+        '7' == =='7':
+            write(data())
+        '8' == =='8':
+            print('До свидания!')
+            break
